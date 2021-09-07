f6edfebe23750cae803dfae3bccf276d0efd4815
clay <claytonharting@gmail.com>
2021-09-07 09:49:53 -0400

    feat: added script for light manual testing on LParse

---
+++ LParseTest.cs
@@ -0,0 +1,26 @@
+﻿using LGen.LParse;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LParseTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LSystem s = new LSystem
+        (
+            "A"+"\n"+
+            "A => AB"+"\n"+
+            "B => CA"
+        );
+        
+        Debug.Log( s.Generate(new GrowthProfile(3), new Randomizer()) );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
