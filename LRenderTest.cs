0f5f7770a3e76e82efb43ce745bd18e6d588ac24
clay <claytonharting@gmail.com>
2021-09-07 13:15:18 -0400

    feat: upgraded test so that it applies a lit material to the branches and set the camera so the results are easier to see

--- LRenderTest.cs
+++ LRenderTest.cs
@@ -6,11 +6,15 @@ using UnityEngine;
 
 public class LRenderTest : MonoBehaviour
 {
+    public Material mat;
+
     // Start is called before the first frame update
     void Start()
     {
+        //string s = "A^^A++++A";
+        string s = "A^[AA^A^A]A_A____A^A";
         Modeller modeller = new Modeller();
-        List<Mesh> branches = modeller.GenerateMeshes(new Sentence("AAA"));
+        List<Mesh> branches = modeller.GenerateMeshes(new Sentence(s));
         
         foreach(Mesh m in branches)
         {
@@ -20,6 +24,7 @@ public class LRenderTest : MonoBehaviour
             MeshRenderer meshRenderer = g.AddComponent<MeshRenderer>();
             MeshFilter meshFilter = g.AddComponent<MeshFilter>();
             meshFilter.mesh = m;
+            meshRenderer.material = mat;
         }
     }
 
