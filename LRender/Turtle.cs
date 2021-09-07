09a205aa088eb088af32c6c7b1bab0fc513875f1
clay <claytonharting@gmail.com>
2021-09-07 12:35:39 -0400

    feat: created LRender module with capability to create meshes for the branch structure of a given sentence, not yet tested

---
+++ LRender/Turtle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace LGen.LRender 
+{ 
+
+    public class Turtle
+    {
+        public Vertex location;
+
+        public Vertex Forwards(float distance)
+        {
+            Vector3 dxyz = Utils.PolarToCartesian(distance, this.location.yaw, this.location.pitch);
+            this.location.x += dxyz.x;
+            this.location.y += dxyz.y;
+            this.location.z += dxyz.z;
+
+            return Vertex.Clone(this.location);
+        }
+
+        public Vertex GetVertexClone() { return Vertex.Clone(location); }
+
+    }
+}
---
+++ LRender/Utils.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace LGen.LRender 
+{ 
+    public class Utils
+    {
+        public static Vector3 PolarToCartesian(float distance, float yaw, float pitch) 
+        { 
+            // distance, yaw, pitch - where yaw and pitch are in radians
+
+            float r = distance;
+            float theta = yaw;
+            float phi = pitch;
+
+            return new Vector3
+            (
+                r * Mathf.Cos(phi) * Mathf.Sin(theta),
+                r * Mathf.Sin(phi) * Mathf.Sin(theta),
+                r * Mathf.Cos(theta)
+            );
+        }
+    }
+}
