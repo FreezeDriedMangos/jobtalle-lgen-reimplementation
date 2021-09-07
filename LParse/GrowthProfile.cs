be9643b65aed77f823840d4b0ced6d3efdc80ef6
FreezeDriedMangoes <claytonharting+github@gmail.com>
2021-09-07 09:43:13 -0400

    feat: initial port of jobtalle's LParse. all files included, but not yet tested

---
+++ LParse/GrowthProfile.cs
@@ -0,0 +1,12 @@
+﻿
+namespace LGen.LParse 
+{ 
+    public class GrowthProfile
+    {
+        public int Iterations { get; private set; }
+
+        public GrowthProfile(int iterations) { this.Iterations = iterations; }
+
+        public virtual int GetGrowth(int iteration) { return 0; }
+    }
+}
---
+++ LParse/Rule.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+namespace LGen.LParse 
+{ 
+    public class Rule
+    {
+        
+        public const string CONNECTIVE = " => ";
+
+        public Sentence LHS { get; private set; }
+        public Sentence RHS { get; private set; }
+
+        public Rule(Sentence LHS, Sentence RHS) { this.LHS = LHS; this.RHS = RHS; }
+        public Rule(string s)
+        {
+            string[] strings = s.Split(new string[]{CONNECTIVE}, System.StringSplitOptions.RemoveEmptyEntries);
+            if (strings.Length != 2) throw new Exception("Cannot construct rule from invalid rule string.");
+
+            this.LHS = strings[0];
+            this.RHS = strings[1];
+        }
+
+        
+        public static bool operator ==(Rule r, Rule u) => r.Equals(u);
+        public static bool operator !=(Rule r, Rule u) => !r.Equals(u);
+        public static implicit operator Rule(string s) => new Rule(s);
+        public bool Equals(Rule other) { return this.LHS == other.LHS && this.RHS == other.RHS; }
+        public override string ToString() { return this.LHS + CONNECTIVE + this.RHS; }
+    }
+}
