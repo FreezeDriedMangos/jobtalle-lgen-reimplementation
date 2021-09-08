a6db726f7589b1c764c594ab7544cbdffc9d3907
FreezeDriedMangoes <claytonharting+github@gmail.com>
2021-09-08 12:34:22 -0400

    feat: tweaked mutation changes

--- LEvolve/MutationProfile.cs
+++ LEvolve/MutationProfile.cs
@@ -11,8 +11,8 @@
         //
 
         public float duplicateRuleChance = 0.01f;
-        public float deleteRuleChance = 0.01f;
-        public float createNewRuleChance = 0.01f;
+        public float deleteRuleChance = 0.001f;
+        public float createNewRuleChance = 0.001f;
 
         // meta
         public float chooseEForNewRuleLHSChance = 1f;
@@ -22,15 +22,15 @@
         //
 
         // chances for each type of mutation to occur
-        public float addSymbolChance = 0.01f;
+        public float addSymbolChance = 0.02f;
         public float removeSymbolChance = 0.01f;
-        public float addBranchChance = 0.01f;
+        public float addBranchChance = 0.005f;
         public float removeBranchChance = 0.01f;
-        public float addLeafChance = 0.01f;
+        public float addLeafChance = 0.005f;
         public float removeLeafChance = 0.01f;
 
         // meta stuff - these are only accessed while processing a mutation
         public float addSymbolBehindChance = 0.5f;
-        public float useEChance = 0.5f;
+        public float useEChance = 0.8f;
     }
 }
