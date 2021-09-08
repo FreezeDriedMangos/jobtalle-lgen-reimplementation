560f33b533a9f4fb041b74659952f174d9d39fe6
clay <claytonharting@gmail.com>
2021-09-08 12:11:12 -0400

    feat: set up test for LEvolve

---
+++ LEvolveTest.cs
@@ -0,0 +1,36 @@
+﻿using LGen.LEvolve;
+using LGen.LParse;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LEvolveTest : MonoBehaviour
+{
+    public string axiom;
+    public string[] rules;
+
+    public int seed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LSystem s = new LSystem(axiom, rules);
+        Mutator m = new Mutator();
+        MutationProfile p = new MutationProfile();
+        Randomizer r  = new Randomizer(seed);
+
+        Debug.Log(s);
+        
+        s = m.Mutate(s, p, r);
+        Debug.Log(s);
+        
+        for(int i = 0; i < 100; i++) s = m.Mutate(s, p, r);
+        Debug.Log(s);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
