8f6d8bf28cddfaa21bb2972112bdca827d70af86
clay <claytonharting@gmail.com>
2021-09-08 16:09:16 -0400

    feat: restructured mutation to work like Job Talle's method

--- LEvolve/MutationProfile.cs
+++ LEvolve/MutationProfile.cs
@@ -3,41 +3,12 @@
     public class MutationProfile
     {
         // misc
-        public int sentenceSizeLimit = 10;
-        public int NDopingAmount = 10;
-
-        //
-        // System level mutations
-        //
-
-        public float duplicateRuleChance = 0.01f;
-        public float deleteRuleChance = 0.001f;
-        public float createNewRuleChance = 0.001f;
-
-        // meta
-        public float chooseEForNewRuleLHSChance = 1f;
-
-        //
-        // Sentence-level mutations
-        //
-
-        // chances for each type of mutation to occur
-        public float addSymbolChance = 0.02f;
-        public float removeSymbolChance = 0.005f;
-        public float addBranchChance = 0.005f;
-        public float removeBranchChance = 0.0025f;
-        public float addLeafChance = 0.005f;
-        public float removeLeafChance = 0.0025f;
-        
-        // meta stuff - these are only accessed while processing a mutation
-        public float addSymbolBehindChance = 0.5f;
-        public float useEChance = 0.8f;
-
-
+        public int sentenceSizeLimit = 32;
 
 		public float pSymbolRemove = 0.005f;
 		public float pSymbolAdd = 0.005f;
 		public    float pSymbolChanceNew = 0.2f; // chance to use N instead of E
+		public    float pAddSymbolBehind = 0.5f;
 		public       float pSymbolChanceRotation = 0.35f; // These probabilities are
 		public       float pSymbolChanceSeed = 0.05f;     // relative to each other,
 		public       float pSymbolChanceStep = 0.4f;      // their total probability
