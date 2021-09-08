072bc34dce2f8cc4268314ab0899fd050412f6bf
FreezeDriedMangoes <claytonharting+github@gmail.com>
2021-09-08 12:49:31 -0400

    chore: added mutation profile values from source repo

--- LEvolve/MutationProfile.cs
+++ LEvolve/MutationProfile.cs
@@ -23,14 +23,31 @@
 
         // chances for each type of mutation to occur
         public float addSymbolChance = 0.02f;
-        public float removeSymbolChance = 0.01f;
+        public float removeSymbolChance = 0.005f;
         public float addBranchChance = 0.005f;
-        public float removeBranchChance = 0.01f;
+        public float removeBranchChance = 0.0025f;
         public float addLeafChance = 0.005f;
-        public float removeLeafChance = 0.01f;
+        public float removeLeafChance = 0.0025f;
         
         // meta stuff - these are only accessed while processing a mutation
         public float addSymbolBehindChance = 0.5f;
         public float useEChance = 0.8f;
     }
 }
+
+// from Job Talle's repo:
+// float pSymbolAdd = 0.005f,
+// float pSymbolRemove = 0.005f,
+// float pSymbolChanceNew = 0.2f,
+// float pSymbolChanceRotation = 0.35f, // These probabilities are
+// float pSymbolChanceSeed = 0.05f,     // relative to each other,
+// float pSymbolChanceStep = 0.4f,      // their total probability
+// float pSymbolChanceConstant = 0.2f,  // is 100%.
+// float pSymbolChanceLeaf = 0.0f,      //
+// float pBranchAdd = 0.002f,
+// float pBranchRemove = 0.002f,
+// float pLeafAdd = 0.002f,
+// float pLeafRemove = 0.002f,
+// float pRuleDuplicate = 0.003f,
+// float pRuleAdd = 0.001f,
+// float pRuleRemove = 0.004f
