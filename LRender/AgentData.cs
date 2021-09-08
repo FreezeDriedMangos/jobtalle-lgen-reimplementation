3eb59df9a3949f1c62bf8a3d1f4d1c48dd887202
clay <claytonharting@gmail.com>
2021-09-08 18:00:13 -0400

    feat: added reports and a more complete modeller

---
+++ LRender/AgentData.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LGen.LRender
+{
+    public class AgentData
+    {
+        public AgentMeshes meshes = new AgentMeshes();
+
+        public List<SeedReport> seedReports = new List<SeedReport>(); 
+        public List<LeafReport> leafReports = new List<LeafReport>();
+        public LimitsReport limitsReport = new LimitsReport();
+        public ExposureReport exposureReport = new ExposureReport();
+        public PositionReport positionReport = new PositionReport();
+    }
+
+    public class AgentMeshes
+    {
+        public List<Mesh> stemMeshes = new List<Mesh>();
+        public List<Mesh> seedMeshes = new List<Mesh>();
+        public List<Mesh> leafMeshes = new List<Mesh>();
+    }
+}
