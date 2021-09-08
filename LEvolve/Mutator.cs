fd9cd7ff699c069da22efd116204e90729b3f95b
FreezeDriedMangoes <claytonharting+github@gmail.com>
2021-09-08 16:09:16 -0400

    feat: restructured mutation to work like Job Talle's method

--- LEvolve/Mutator.cs
+++ LEvolve/Mutator.cs
@@ -11,50 +11,49 @@ namespace LGen.LEvolve
         public LSystem Mutate(LSystem system, MutationProfile profile, Randomizer randomizer)
         {
             system = new LSystem(system);
-
-            List<Token> E = GetESet(system);
-            List<Token> N = GetNSet(profile);
-            Sentence axiom = Mutate(system.Axiom, profile, randomizer, E, N);
+            
+            GeneratedSymbols E = new GeneratedSymbols(system);
+            Sentence axiom = Mutate(system.Axiom, profile, randomizer, E);
             
             for(int i = 0; i < system.Rules.Count; i++)
             {
-                system.Rules[i] = Mutate(system.Rules[i], profile, randomizer, E, N);
+                system.Rules[i] = Mutate(system.Rules[i], profile, randomizer, E);
 
-                if (randomizer.MakeFloat(0,1) < profile.duplicateRuleChance) system.Rules.Add(new Rule(system.Rules[i]));
-                if (randomizer.MakeFloat(0,1) < profile.deleteRuleChance)    system.Rules.RemoveAt(i--); // the -- is here because otherwise we'd skip over the next rule
-                if (randomizer.MakeFloat(0,1) < profile.createNewRuleChance) system.Rules.Add(new Rule("", ""));
+                if (randomizer.MakeFloat(0,1) < profile.pRuleDuplicate) system.Rules.Add(new Rule(system.Rules[i]));
+                if (randomizer.MakeFloat(0,1) < profile.pRuleRemove)    system.Rules.RemoveAt(i--); // the -- is here because otherwise we'd skip over the next rule
+                if (randomizer.MakeFloat(0,1) < profile.pRuleAdd)       system.Rules.Add(new Rule("", ""));
             }
 
             return system;
         }
 
-        public Rule Mutate(Rule rule, MutationProfile profile, Randomizer randomizer, List<Token> E, List<Token> N)
+        private Rule Mutate(Rule rule, MutationProfile profile, Randomizer randomizer, GeneratedSymbols E)
         {
-            Sentence lhs = Mutate(rule.LHS, profile, randomizer, E, N, true);
-            Sentence rhs = Mutate(rule.RHS, profile, randomizer, E, N, false);
+            Sentence lhs = Mutate(rule.LHS, profile, randomizer, E, true);
+            Sentence rhs = Mutate(rule.RHS, profile, randomizer, E, false);
             return new Rule(lhs, rhs);
         }
 
-        public Sentence Mutate(Sentence sentence, MutationProfile profile, Randomizer randomizer, List<Token> E, List<Token> N, bool isRuleLHS = false)
+        private Sentence Mutate(Sentence sentence, MutationProfile profile, Randomizer randomizer, GeneratedSymbols E, bool isRuleLHS = false)
         {
             Sentence oldSentence = sentence;
             sentence = new Sentence(sentence);    
             
             if (sentence.Tokens.Count == 0)
             {
-                if (randomizer.MakeFloat(0,1) < profile.addSymbolChance)    AddSymbolAt(sentence, 0, profile, randomizer, E, N, isRuleLHS, true);
-                if (randomizer.MakeFloat(0,1) < profile.addBranchChance)    sentence.Tokens.AddRange(new Token[]{ Legend.BRANCH_OPEN, Legend.BRANCH_CLOSE });
-                if (randomizer.MakeFloat(0,1) < profile.addLeafChance)      sentence.Tokens.AddRange(new Token[]{ Legend.LEAF_OPEN, Legend.BRANCH_CLOSE });
+                if (randomizer.MakeFloat(0,1) < profile.pSymbolAdd)    AddSymbolAt(sentence, 0, profile, randomizer, E, isRuleLHS, true);
+                if (randomizer.MakeFloat(0,1) < profile.pBranchAdd)    sentence.Tokens.AddRange(new Token[]{ Legend.BRANCH_OPEN, Legend.BRANCH_CLOSE });
+                if (randomizer.MakeFloat(0,1) < profile.pLeafAdd)      sentence.Tokens.AddRange(new Token[]{ Legend.LEAF_OPEN, Legend.BRANCH_CLOSE });
             }
 
             for(int i = 0; i < sentence.Tokens.Count; i++)
             {
-                if (randomizer.MakeFloat(0,1) < profile.addSymbolChance)    AddSymbolAt(sentence, i, profile, randomizer, E, N, isRuleLHS);
-                if (randomizer.MakeFloat(0,1) < profile.removeSymbolChance) RemoveSymbolAt(sentence, i);
-                if (randomizer.MakeFloat(0,1) < profile.addBranchChance)    AddBranchAt(sentence, i);
-                if (randomizer.MakeFloat(0,1) < profile.removeBranchChance) RemoveBranchOrLeafAt(sentence, i);
-                if (randomizer.MakeFloat(0,1) < profile.addLeafChance)      AddLeafAt(sentence, i);
-                if (randomizer.MakeFloat(0,1) < profile.removeLeafChance)   RemoveBranchOrLeafAt(sentence, i);
+                if (randomizer.MakeFloat(0,1) < profile.pSymbolAdd)    AddSymbolAt(sentence, i, profile, randomizer, E, isRuleLHS);
+                if (randomizer.MakeFloat(0,1) < profile.pSymbolRemove) RemoveSymbolAt(sentence, i);
+                if (randomizer.MakeFloat(0,1) < profile.pBranchAdd)    AddBranchAt(sentence, i);
+                if (randomizer.MakeFloat(0,1) < profile.pBranchRemove) RemoveBranchAt(sentence, i);
+                if (randomizer.MakeFloat(0,1) < profile.pLeafAdd)      AddLeafAt(sentence, i);
+                if (randomizer.MakeFloat(0,1) < profile.pLeafRemove)   RemoveLeafAt(sentence, i);
             }
 
             return sentence.Tokens.Count > profile.sentenceSizeLimit ? oldSentence : sentence;
@@ -64,61 +63,45 @@ namespace LGen.LEvolve
         // Utils
         //
 
-        private List<Token> GetESet(LSystem system)
+        private Token MakeToken(Randomizer randomizer, MutationProfile profile, GeneratedSymbols E = null)
         {
-            // add all non-branch tokens from system.axiom and system.rules.map(r => r.RHS)
+            float rand = randomizer.MakeFloat(0, 1);
 
-            List<Token> E = new List<Token>();
-            List<Token> allTokens = new List<Token>();
-            allTokens.AddRange(system.Axiom.Tokens);
-            foreach(Rule r in system.Rules) allTokens.AddRange(r.RHS.Tokens);
+            if (rand < profile.pSymbolChanceRotation)
+                return E == null
+                    ? new Token[] { 
+                        Legend.PITCH_INCREMENT, 
+                        Legend.PITCH_DECREMENT, 
+                        Legend.ROLL_INCREMENT, 
+                        Legend.ROLL_DECREMENT, 
+                        Legend.YAW_INCREMENT, 
+                        Legend.YAW_DECREMENT, 
+                        }[randomizer.MakeInt_Exclusive(0, 6)]
+                    : E.rotations.Count == 0 ? null : E.rotations[randomizer.MakeInt_Exclusive(0, E.rotations.Count)]
+                ;
+            rand -= profile.pSymbolChanceRotation;
 
-            foreach(Token t in allTokens) 
-            {
-                if(t == Legend.BRANCH_OPEN)  continue;
-                if(t != Legend.BRANCH_CLOSE) continue;
-                if(t != Legend.LEAF_OPEN)    continue;
-
-                E.Add(t);
-            }
-
-            return E;
-        }
-
-        private List<Token> GetNSet(MutationProfile profile)
-        {
-            List<Token> N = new List<Token>();
+            if (rand < profile.pSymbolChanceSeed)
+                return E == null
+                    ? Legend.SEED
+                    : E.seeds.Count == 0 ? null : E.seeds[randomizer.MakeInt_Exclusive(0, E.seeds.Count)]
+                ;
+            rand -= profile.pSymbolChanceSeed;
             
-            for(char t = Legend.STEP_MIN; t < Legend.STEP_MAX; t++) N.Add(t);
-            for(char t = Legend.CONST_MIN; t < Legend.CONST_MAX; t++) N.Add(t);
-            N.Add(Legend.SEED            );
-            N.Add(Legend.PITCH_INCREMENT );
-            N.Add(Legend.PITCH_DECREMENT );
-            N.Add(Legend.ROLL_INCREMENT  );
-            N.Add(Legend.ROLL_DECREMENT  );
-            N.Add(Legend.YAW_INCREMENT   );
-            N.Add(Legend.YAW_DECREMENT   );
-            
-            List<Token> R = GetRSet();
-            for(int i = 0; i < profile.NDopingAmount; i++)
-            {
-                N.AddRange(R);
-                N.Add(Legend.SEED);
-            }
+            if (rand < profile.pSymbolChanceStep)
+                return E == null
+                    ? (char)randomizer.MakeInt_Inclusive(Legend.STEP_MIN, Legend.STEP_MAX)
+                    : E.steps.Count == 0 ? null : E.steps[randomizer.MakeInt_Exclusive(0, E.steps.Count)]
+                ;
+            rand -= profile.pSymbolChanceStep;
 
-            return N;
-        }
+            if (true)
+                return E == null
+                    ? (char)randomizer.MakeInt_Inclusive(Legend.CONST_MIN, Legend.CONST_MAX)
+                    : E.constants.Count == 0 ? null : E.constants[randomizer.MakeInt_Exclusive(0, E.constants.Count)]
+                ;
 
-        private List<Token> GetRSet()
-        {
-            List<Token> R = new List<Token>();
-            R.Add(Legend.ROLL_INCREMENT );
-            R.Add(Legend.ROLL_DECREMENT );
-            R.Add(Legend.PITCH_INCREMENT);
-            R.Add(Legend.PITCH_DECREMENT);
-            R.Add(Legend.YAW_INCREMENT  );
-            R.Add(Legend.YAW_DECREMENT  );
-            return R;
+            throw new Exception("Token failed to generate!");
         }
 
         private bool InsideLeaf(Sentence sentence, int i)
@@ -179,12 +162,13 @@ namespace LGen.LEvolve
         // Sentence mutations
         //
 
-        private void AddSymbolAt(Sentence sentence, int i, MutationProfile profile, Randomizer randomizer, List<Token> E, List<Token> N, bool forceE = false, bool forceBehind = false)
+        private void AddSymbolAt(Sentence sentence, int i, MutationProfile profile, Randomizer randomizer, GeneratedSymbols E, bool forceE = false, bool forceBehind = false)
         {
-            int insertAt = (forceBehind || randomizer.MakeFloat(0, 1) < profile.addSymbolBehindChance) ? i : i+1;
-            List<Token> set = (forceE || randomizer.MakeFloat(0, 1) < profile.useEChance) ? E : N;
-            if (set.Count == 0) return;
-            sentence.Tokens.Insert(insertAt, set[randomizer.MakeInt_Exclusive(0, set.Count)]);
+            int insertAt = (forceBehind || randomizer.MakeFloat(0, 1) < profile.pAddSymbolBehind) ? i : i+1;
+            GeneratedSymbols useE = (forceE || randomizer.MakeFloat(0, 1) < 1-profile.pSymbolChanceNew) ? E : null;
+            Token token = MakeToken(randomizer, profile, useE);
+            if (token == null) return;
+            sentence.Tokens.Insert(insertAt, token);
         }
 
         private void RemoveSymbolAt(Sentence sentence, int i)
@@ -227,6 +211,14 @@ namespace LGen.LEvolve
             }
         }
 
+        private void AddLeafAt(Sentence sentence, int i)
+        { 
+            // if inside leaf, do not add a sub leaf, that's not allowed
+            if (InsideLeaf(sentence, i)) return;
+
+            AddBranchAt(sentence, i, Legend.LEAF_OPEN);
+        }
+
         private void RemoveBranchOrLeafAt(Sentence sentence, int i)
         {
             // if not branch open, fail
@@ -237,13 +229,61 @@ namespace LGen.LEvolve
             sentence.Tokens.RemoveAt(close);
             sentence.Tokens.RemoveAt(open);
         }
-        
-        private void AddLeafAt(Sentence sentence, int i)
-        { 
-            // if inside leaf, do not add a sub leaf, that's not allowed
-            if (InsideLeaf(sentence, i)) return;
 
-            AddBranchAt(sentence, i, Legend.LEAF_OPEN);
+        private void RemoveBranchAt(Sentence sentence, int i)
+        {
+            if (sentence.Tokens[i] != Legend.BRANCH_OPEN) return;
+            RemoveBranchOrLeafAt(sentence, i);
+        }
+
+        private void RemoveLeafAt(Sentence sentence, int i)
+        {
+            if (sentence.Tokens[i] != Legend.LEAF_OPEN) return;
+            RemoveBranchOrLeafAt(sentence, i);
+        }
+        
+    }
+
+    
+    class GeneratedSymbols
+    {
+        public List<Token> rotations = new List<Token>();
+        public List<Token> seeds = new List<Token>();
+	    public List<Token> steps = new List<Token>();
+        public List<Token> constants = new List<Token>();
+
+        public GeneratedSymbols(LSystem system)
+        {
+            List<Token> allTokens = new List<Token>();
+            allTokens.AddRange(system.Axiom.Tokens);
+            foreach (Rule r in system.Rules) allTokens.AddRange(r.RHS.Tokens);
+
+            foreach (Token t in allTokens)
+            {
+                if (t == Legend.BRANCH_OPEN) continue;
+                if (t != Legend.BRANCH_CLOSE) continue;
+                if (t != Legend.LEAF_OPEN) continue;
+
+                if (t.OnRangeInclusive(Legend.CONST_MIN, Legend.CONST_MAX)) this.constants.Add(t);
+                if (t.OnRangeInclusive(Legend.STEP_MIN, Legend.STEP_MAX)) this.steps.Add(t);
+                if (t == Legend.SEED) this.seeds.Add(t);
+                if 
+                (
+                    t == Legend.PITCH_INCREMENT ||
+                    t == Legend.PITCH_DECREMENT ||
+                    t == Legend.ROLL_INCREMENT ||
+                    t == Legend.ROLL_DECREMENT ||
+                    t == Legend.YAW_INCREMENT ||
+                    t == Legend.YAW_DECREMENT
+                ) this.rotations.Add(t);
+            }
+
+            //if (this.constants.Count == 0) this.constants.Add(Legend.CONST_MIN);
+            //if (this.steps.Count == 0) this.steps.Add(Legend.STEP_MIN);
+            //if (this.seeds.Count == 0) this.seeds.Add(Legend.SEED);
+            //if (this.rotations.Count == 0) this.rotations.Add(Legend.YAW_INCREMENT);
         }
     }
 }
+
+
