5cfe533de1a9e19c13e2b315432b9b4e9a9f50a9
FreezeDriedMangoes <claytonharting+github@gmail.com>
2021-09-09 14:58:15 -0400

    feat: Created the Renderer class and the shader/materials/infrastructure necessary to start evaluating leaf exposure

---
+++ LRender/RendererResources.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace LGen.LRender
+{
+    public class RendererResources : Singleton<RendererResources>
+    {
+        public Material leafExposureMaterial;
+        public Material stemExposureMaterial;
+        public Material seedExposureMaterial;
+    }
+}
