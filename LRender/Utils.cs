ecc4a2d181e2429432fcacfa6b4359b4c8de5c51
FreezeDriedMangoes <claytonharting+github@gmail.com>
2021-09-09 15:45:17 -0400

    feat: fixed mesh generation for stems pointing straight up

--- LRender/Utils.cs
+++ LRender/Utils.cs
@@ -4,20 +4,9 @@ namespace LGen.LRender
 { 
     public class Utils
     {
-        public static Vector3 PolarToCartesian(float distance, float yaw, float pitch) 
-        { 
-            // distance, yaw, pitch - where yaw and pitch are in radians
-
-            float r = distance;
-            float theta = yaw;
-            float phi = pitch;
-
-            return new Vector3
-            (
-                r * Mathf.Cos(phi) * Mathf.Sin(theta),
-                r * Mathf.Sin(phi) * Mathf.Sin(theta),
-                r * Mathf.Cos(theta)
-            );
+        public static Vector3 ApplyRollPitchYaw(float roll, float pitch, float yaw, Vector3 vec)
+        {
+            return Quaternion.Euler(new Vector3(Mathf.Rad2Deg*pitch, Mathf.Rad2Deg*yaw, Mathf.Rad2Deg*roll)) * vec;
         }
     }
 }
