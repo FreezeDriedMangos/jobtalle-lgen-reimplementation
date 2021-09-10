926d2eb3c41076e1c6b4054e6490ac01cc986078
FreezeDriedMangoes <claytonharting+github@gmail.com>
2021-09-10 17:25:41 -0400

    feat: added a camera lock to make using the ui easier. toggle it with LeftControl

--- CameraMove.cs
+++ CameraMove.cs
@@ -25,7 +25,7 @@ public class CameraMove : MonoBehaviour
     float camSens = 0.25f; //How sensitive it with mouse
     private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
     private float totalRun = 1.0f;
-    private bool cursorLocked = false; 
+    private bool cursorLocked = true; 
 
     void Update () {
         lastMouse = Input.mousePosition - lastMouse ;
