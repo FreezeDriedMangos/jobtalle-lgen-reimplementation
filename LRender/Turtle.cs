42e147d04e2e46b802ffe60bb746d6fd152430f8
clay <claytonharting@gmail.com>
2021-09-10 08:16:34 -0400

    feat: added a seed offset so that seeds can be placed on the sides of branches instead of embedded inside

--- LRender/Turtle.cs
+++ LRender/Turtle.cs
@@ -26,5 +26,18 @@ namespace LGen.LRender
 
         public Vertex GetVertexClone() { return Vertex.Clone(location); }
 
+        public Vector3 GetSideways(float distance)
+        {
+            Vector3 dxyz = distance * Utils.ApplyRollPitchYaw(location.roll, location.pitch, location.yaw, Vector3.right);
+            
+            Vertex v = GetVertexClone();
+            
+            return new Vector3(
+                this.location.x + dxyz.x,
+                this.location.y + dxyz.y,
+                this.location.z + dxyz.z
+            );
+        }
+
     }
 }
