476cd87c56acf737f017ce550cb57b9b0605a56c
FreezeDriedMangoes <claytonharting+github@gmail.com>
2021-09-10 12:39:20 -0400

    feat: added a ui to run the simulation

---
+++ ButtonController.cs
@@ -0,0 +1,18 @@
+﻿using LGen.LSimulate;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    public UnityEngine.UI.InputField advanceIterationsInput;
+
+    public void AdvanceIterations()
+    {
+        int numIterations = int.Parse(advanceIterationsInput.text);
+        for (int i = 0; i < numIterations; i++)
+        {
+            Simulation.Instance.Iterate_SecondHalfFirst(Simulation.Instance.state);
+        }
+    }
+}
