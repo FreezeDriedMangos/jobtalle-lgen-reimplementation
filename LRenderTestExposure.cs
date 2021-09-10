5c486e3be670f1f3fe0e42c5801ea9277f273e12
clay <claytonharting@gmail.com>
2021-09-10 08:59:46 -0400

    feat: set up tests and scene to prepare for exposure calculation tests

---
+++ LRenderTestExposure.cs
@@ -0,0 +1,34 @@
+﻿using LGen.LParse;
+using LGen.LRender;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LRenderTestExposure : MonoBehaviour
+{
+    public List<string> agents;
+    public List<Vector3> agentPositions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<AgentRenderData> agentData = new List<AgentRenderData>();
+        LGen.LRender.Renderer r = new LGen.LRender.Renderer();
+        Randomizer rand = new Randomizer();
+
+        for(int i = 0; i < agents.Count; i++)
+        {
+            AgentRenderData d = r.Render(agents[i], rand, i);
+            d.gameObject.transform.position = agentPositions[i];
+        }
+
+        r.EvaluateExposure(agentData);
+        Debug.Log(agentData.ConvertAll(new System.Converter<AgentRenderData, float>((AgentRenderData d) => d.agentData.exposureReport.exposure)).ToArray());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
