92ff3da0c4fe9c3437b31c7a86aba5335f5c61cd
clay <claytonharting@gmail.com>
2021-09-10 09:02:35 -0400

    fix: forgot to add each agent render to the list of agent renders

--- LRenderTestExposure.cs
+++ LRenderTestExposure.cs
@@ -19,11 +19,14 @@ public class LRenderTestExposure : MonoBehaviour
         for(int i = 0; i < agents.Count; i++)
         {
             AgentRenderData d = r.Render(agents[i], rand, i);
+            agentData.Add(d);
             d.gameObject.transform.position = agentPositions[i];
         }
 
         r.EvaluateExposure(agentData);
-        Debug.Log(agentData.ConvertAll(new System.Converter<AgentRenderData, float>((AgentRenderData d) => d.agentData.exposureReport.exposure)).ToArray());
+        List<float> exposures = agentData.ConvertAll(new System.Converter<AgentRenderData, float>((AgentRenderData d) => d.agentData.exposureReport.exposure));
+        Debug.Log(exposures.Count);
+        Debug.Log("[" + string.Join(", ", exposures) + "]");
     }
 
     // Update is called once per frame
