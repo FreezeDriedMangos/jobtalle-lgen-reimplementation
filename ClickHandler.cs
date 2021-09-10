46c13c004a6ee32716f0db0671aeb6f16593eda0
clay <claytonharting@gmail.com>
2021-09-10 15:02:53 -0400

    feat: added infrastructure to support for clicking on agents in the simulation

---
+++ ClickHandler.cs
@@ -0,0 +1,28 @@
+﻿using LGen.LSimulate;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // code from https://answers.unity.com/questions/1605687/click-on-mesh.html
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Simulation.Instance.PrintAgentForGameObject(hit.collider.gameObject);
+            }
+        }
+    }
+}
