8894a0739ea0710fac96c1f21058dd4b09033619
FreezeDriedMangoes <claytonharting+github@gmail.com>
2021-09-13 09:16:39 -0400

    feat: replaced seed sphere mesh with a cube mesh, this 10x'd the performance of the agent rendering function

---
+++ TimeKeeper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeKeeper : Singleton<TimeKeeper>
+{
+    
+    public string BuildSentences;
+    public string RenderAgents;
+    public string EvaluateLeafExposure;
+    public string EvaluateViability;
+    public string SeedNextState;
+
+    //BuildAgentSentences(state);
+    //RenderAgents_AndEvaluateLeafExposure(state);
+    //EvaluateAgentsViability(state);            
+    //SeedNextState
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
